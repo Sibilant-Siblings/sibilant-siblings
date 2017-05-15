19d680194955918078cfa010dd2f037f6210b889
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-15 11:35:44 -0700

    "changed db url to localhost again"

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 
 var connection = mysql.createConnection({
-  host: 'mysql://ba3792a798d949:75eb8e04@us-cdbr-iron-east-03.cleardb.net/heroku_f5c8b8d3702a6c0?reconnect=true',
+  host: 'localhost',
   user: 'root',
   password: '',
   database: 'foodQuest',
