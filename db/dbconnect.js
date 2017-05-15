07c1d39a20671fefe15ef74e93ca801177e7174e
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-15 11:51:33 -0700

    re-did db connection to use heroku urls and user/pass

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 
 var connection = mysql.createConnection({
-  host: CLEARDB_DATABASE_URL,
-  user: 'root',
-  password: '',
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'ba3792a798d949',
+  password: '75eb8e04',
   database: 'foodQuest',
   multipleStatements: true
 });
