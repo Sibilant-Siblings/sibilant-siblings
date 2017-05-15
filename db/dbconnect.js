54dbe726b5aa048d73580265a155a13b7612a2ca
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-15 11:48:22 -0700

    "cleardb attempt"

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 // and to the database "chat".
 
 var connection = mysql.createConnection({
-  host: DATABASE_URL,
+  host: CLEARDB_DATABASE_URL,
   user: 'root',
   password: '',
   database: 'foodQuest',
