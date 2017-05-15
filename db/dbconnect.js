5ab72977097883160e2f7c0b35c4ddbf47f7bd01
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-15 11:55:31 -0700

    "another connection change attempt adding foodquest to host url"

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 
 var connection = mysql.createConnection({
-  host: 'us-cdbr-iron-east-03.cleardb.net',
+  host: 'us-cdbr-iron-east-03.cleardb.net/foodquest',
   user: 'ba3792a798d949',
   password: '75eb8e04',
-  database: 'heroku-foodQuest',
+  database: 'foodquest',
   multipleStatements: true
 });
 
